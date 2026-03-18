76420bde8f8eded6c97603ad27b896e9e11a45f9
bharathkumar-2480352 <Bharathkumar.LNU@cognizant.com>
2026-03-18 17:47:28 +0530

    clean commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,23 @@
+// src/App.jsx
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
 import Login from './pages/Login';
 import Home from './pages/Home';
+// import MyLibrary from './pages/MyLibrary'; // You will create this later!
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Home />} />
+        
+        {/* Everything inside this Layout route gets the Sidebar automatically! */}
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/my-library" element={<MyLibrary />} /> */}
+        </Route>
+        
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/utils/theme.js
@@ -0,0 +1,10 @@
+// src/utils/theme.js
+export const COLORS = {
+  sidebarBg: '#F8F6F1',     
+  mainBg: '#F2EAE0',        
+  activeBg: '#E5D5C5',      
+  textSecondary: '#9C8C7D', 
+  textPrimary: '#2D2D2D',   
+  white: '#FFFFFF',
+  shelfShadow: 'rgba(156, 140, 125, 0.4)'
+};
