c155f02555ea8c06cddb3c00cd9492ad583c8f76
durgabhavanigunda <Gunda.Durgabhavani@cognizant.com>
2026-03-30 15:38:26 +0530

    updated register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,25 @@
 // src/App.jsx
-
-// src/App.jsx
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import MyLibrary from './pages/MyLibrary'; 
+import Profile from './pages/profile';
+import WishList from './pages/WishList';
+import BookBag from './pages/BookBag';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        
-        {/* Everything inside this Layout route gets the Sidebar automatically! */}
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/my-library" element={<MyLibrary />} /> 
-          {/* <Route path="/my-library" element={<MyLibrary />} /> */}
-          <Route path="/profile" element={<Profile/>}/>
-        </Route>
-        
-        {/* Wrapping everything in Layout so the Sidebar is ALWAYS there */}
-        <Route element={<Layout />}>
           <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Home />} />
-          
-          {/* Default redirect to login if not authenticated */}
-          <Route path="*" element={<Navigate to="/login" />} />
+          <Route path="/my-library" element={<MyLibrary />} /> 
+          <Route path="/profile" element={<Profile/>}/>
+          <Route path="/wishlist" element={<WishList />} />
+          <Route path="/cart" element={<BookBag />} />
         </Route>
       </Routes>
     </BrowserRouter>
