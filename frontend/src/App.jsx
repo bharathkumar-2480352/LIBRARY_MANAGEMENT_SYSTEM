1f5df9375b40baf3234c9a57756e0a9ce18b254b
durgabhavanigunda <Gunda.Durgabhavani@cognizant.com>
2026-03-30 15:38:26 +0530

    updated register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
-
 // src/App.jsx
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import MyLibrary from './pages/MyLibrary'; 
+import Profile from './pages/profile';
+import WishList from './pages/WishList';
+import BookBag from './pages/BookBag';
 import MyLibrary from './pages/MyLibrary'; 
 import Profile from './pages/profile';
 import WishList from './pages/WishList';
@@ -17,8 +21,11 @@ function App() {
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/my-library" element={<MyLibrary />} /> 
           <Route path="/profile" element={<Profile/>}/>
+          <Route path="/wishlist" element={<WishList />} />
+          <Route path="/cart" element={<BookBag />} />
           <Route path="/wishlist" element={<WishList />} />
           <Route path="/cart" element={<BookBag />} />
         </Route>
